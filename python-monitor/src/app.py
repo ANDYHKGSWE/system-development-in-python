13267000883a3327c654976e9b73176d6bd83072
Andy <andycheung83a@hotmail.com>
2025-10-23 22:30:41 +0200

    feat: add main loop with place holder handlers

--- python-monitor/src/app.py
+++ python-monitor/src/app.py
@@ -1,15 +1,19 @@
-from core.monitor import get_system_status
+from ui.menu import show_main_menu
 
 def main():
-    s = get_system_status()
-    print(s)
+    print("Välkommen till Andys Systemövervakaren")
+    while True:
+      choice = show_main_menu()
+      if choice == 1:
+          print("Starta övervakning")
+      elif choice == 2:
+          print("Lista aktiv övervakning")
+      elif choice == 3:
+          print("skapa larm")
+      elif choice == 4:
+          print("visa larm")
+      elif choice == 5:
+          print("Starta övervakningsläge")
 
 if __name__ == "__main__":
     main()
-
-
-# def main():
-#   print("Välkommen till systemövervakaren ")
-
-# if __name__ == "__main__":
-#     main()
